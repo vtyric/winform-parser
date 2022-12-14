5dc3685943f9c8208099d01e6850efd1f6527a1f
m.batuev <m.batuev@artsofte.ru>
2022-12-15 02:11:12 +0500

    special for seva

--- CurrencyConverter/extenssions/CurrenciesListExtensions.cs
+++ CurrencyConverter/extenssions/CurrenciesListExtensions.cs
@@ -9,5 +9,8 @@ namespace CurrencyConverter.extenssions
     {
         public static bool IsSame(this IEnumerable<Currency> oldCurrencies, IEnumerable<Currency> newCurrencies) =>
             oldCurrencies.All(a => newCurrencies.Any(b => b.Name.SequenceEqual(a.Name) && Math.Abs(a.Amount - b.Amount) < 10e4));
+
+        public static double ConvertTo(this Currency from, Currency to, double count = 1) =>
+            Math.Round(from.Amount * count / to.Amount, 3);
     }
 }
--- Forms/CurrencyConverterForm.cs
+++ Forms/CurrencyConverterForm.cs
@@ -1,5 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
 using System.Windows.Forms;
+using CurrencyConverter.extenssions;
 using CurrencyConverter.models;
 
 namespace CurrencyConverter
@@ -9,18 +12,117 @@ namespace CurrencyConverter
         private readonly Timer timer;
         private readonly CurrencyConverter currencyConverter;
 
-        private List<Currency> currencies;
+        private string from = "usd";
+        private string to = "eur";
+        private double amount = 1;
+
+        private Currency usd;
+        private Currency eur;
+        private Currency rub;
 
         public CurrencyForm()
         {
-            const int requestInterval = 60 * 60;
             InitializeComponent();
+            const int requestInterval = 60 * 60;
             timer = new Timer { Interval = requestInterval };
             timer.Start();
             currencyConverter = new CurrencyConverter();
             timer.Tick += currencyConverter.UpdateCurrencies;
+            currencyConverter.CurrencyRatesChanged += currencies =>
+            {
+                AddTopLabels(currencies);
+                var amountInput = new TextBox
+                {
+                    Location = new Point(44, 246),
+                    Size = new Size(100, 20),
+                    TabIndex = 0,
+                    Text = "1"
+                };
+                var resultInput = new Label
+                {
+                    Location = new Point(613, 246),
+                    Size = new Size(100, 23),
+                    TabIndex = 1,
+                    Text = $"={GetResult()}"
+                };
+                Controls.Add(amountInput);
+                Controls.Add(resultInput);
+                amountInput.TextChanged += (sender, args) =>
+                {
+                    amount = double.TryParse(amountInput.Text, out var newAmount) ? newAmount : 0;
+                    resultInput.Text = $"={GetResult()}";
+                };
+                var fromCombobox = GetComboBox(new object[] { "usd", "eur", "rub" }, 240, "usd");
+                var toCombobox = GetComboBox(new object[] { "eur", "usd", "rub" }, 480, "eur");
+                Controls.Add(fromCombobox);
+                Controls.Add(toCombobox);
+                fromCombobox.TextChanged += (sender, args) =>
+                {
+                    from = fromCombobox.Text;
+                    resultInput.Text = $"={GetResult()}";
+                };
+                toCombobox.TextChanged += (sender, args) =>
+                {
+                    to = toCombobox.Text;
+                    resultInput.Text = $"={GetResult()}";
+                };
+            };
+        }
 
-            currencyConverter.CurrencyRatesChanged += currencies => { this.currencies = currencies; };
+        private ComboBox GetComboBox(object[] names, int x, string initValue)
+        {
+            var comboBox = new ComboBox()
+            {
+                Location = new Point(x, 246),
+                Size = new Size(100, 20),
+                Text = initValue
+            };
+            comboBox.Items.AddRange(names);
+
+            return comboBox;
+        }
+
+        private string GetResult() =>
+            GetCurrencyByName(from).ConvertTo(GetCurrencyByName(to), amount).ToString(CultureInfo.InvariantCulture);
+
+        private Currency GetCurrencyByName(string name)
+        {
+            if (name == "rub")
+                return rub;
+
+            if (name == "eur")
+                return eur;
+
+            return usd;
+        }
+
+        private void AddTopLabels(IReadOnlyList<Currency> currencies)
+        {
+            usd = currencies[0];
+            eur = currencies[1];
+            rub = new Currency { Name = "rub", Amount = 1 };
+
+            var usdLabel = new Label
+            {
+                Text = $"1 {usd.Name} = {usd.ConvertTo(rub)} rub",
+                Location = new Point(115, 94),
+                Size = new Size(140, 140),
+            };
+            var eurLabel = new Label
+            {
+                Text = $"1 {eur.Name} = {eur.ConvertTo(rub)} rub",
+                Location = new Point(347, 94),
+                Size = new Size(140, 140),
+            };
+            var usdEurLabel = new Label
+            {
+                Text = $"1 {usd.Name} = {usd.ConvertTo(eur)} {eur.Name}",
+                Location = new Point(598, 94),
+                Size = new Size(140, 140),
+            };
+            Controls.Add(usdLabel);
+            Controls.Add(eurLabel);
+            Controls.Add(usdEurLabel);
         }
     }
 }
